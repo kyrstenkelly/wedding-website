bb200336ccc2bf49431bb39c06a50e28bd95dc32
Kyrsten Kelly <kyrsten@udacity.com>
2018-09-02 23:05:09 -0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">James &amp; Kyrsten</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
