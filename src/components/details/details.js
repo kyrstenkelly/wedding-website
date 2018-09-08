d25ec12a754bf9a41bb2f50647cb317714240562
Kyrsten Kelly <kyrsten@udacity.com>
2018-09-08 17:00:44 -0500

    Pretty triangles

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Element } from 'react-scroll';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './details.scss';
@@ -24,44 +25,67 @@ class Details extends Component {
     }
   }
 
+  renderTriangles() {
+    return (
+      <div className='triangles'>
+        <div className='triangle triangle-left'/>
+        <div className='triangle triangle-right'/>
+      </div>
+    );
+  }
+
   render() {
+    const triangles = this.renderTriangles();
+
     return (
       <div className='details'>
-        <div className='section section-1'>
-          <h1 className='section-header'>Some Details Here</h1>
+        <Element name="details" className="section section-1">
+          {triangles}
 
-          <p className='section-body'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque cursus tortor, commodo cursus diam placerat vel. Fusce porta nulla sed felis pellentesque, id vestibulum mi mollis. Fusce dignissim lorem nec scelerisque hendrerit. Cras rutrum pharetra dolor vel feugiat. Vivamus eleifend posuere ultrices. Etiam consequat odio a aliquet iaculis. In hac habitasse platea dictumst. Maecenas sed luctus est. Duis bibendum urna a tristique volutpat.
+          <div className='section-content'>
+            <h1 className='section-header'>Details</h1>
 
-            Aliquam elementum vitae elit nec fringilla. Duis rhoncus odio enim, sed dignissim tellus varius iaculis. Aenean suscipit posuere rhoncus. Duis in nisi a nulla gravida aliquam ac viverra magna. Aenean pharetra tincidunt aliquet. Vestibulum tincidunt a tellus a pellentesque. Nam in bibendum velit.
+            <p className='section-body'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque cursus tortor, commodo cursus diam placerat vel. Fusce porta nulla sed felis pellentesque, id vestibulum mi mollis. Fusce dignissim lorem nec scelerisque hendrerit. Cras rutrum pharetra dolor vel feugiat. Vivamus eleifend posuere ultrices. Etiam consequat odio a aliquet iaculis. In hac habitasse platea dictumst. Maecenas sed luctus est. Duis bibendum urna a tristique volutpat.
 
-            Etiam consequat posuere mi vel sodales. Morbi blandit nibh tellus, ac vulputate ipsum posuere nec. Quisque feugiat erat vitae urna imperdiet ultricies. Quisque tempor nisi at quam egestas, non ullamcorper ante faucibus. Aenean pretium mauris sed massa faucibus posuere. Praesent scelerisque lorem odio, id aliquet leo egestas in. Nam sed lectus commodo, sollicitudin ex nec, mollis massa. Maecenas varius ut ex id aliquet. Pellentesque ac sodales purus. Donec vel tortor est. Sed rhoncus ante sodales tellus gravida tincidunt. Nulla pulvinar nisi orci.
-          </p>
-        </div>
+              Aliquam elementum vitae elit nec fringilla. Duis rhoncus odio enim, sed dignissim tellus varius iaculis. Aenean suscipit posuere rhoncus. Duis in nisi a nulla gravida aliquam ac viverra magna. Aenean pharetra tincidunt aliquet. Vestibulum tincidunt a tellus a pellentesque. Nam in bibendum velit.
 
-        <div className='section section-2'>
-          <h1 className='section-header'>More Details Here</h1>
+              Etiam consequat posuere mi vel sodales. Morbi blandit nibh tellus, ac vulputate ipsum posuere nec. Quisque feugiat erat vitae urna imperdiet ultricies. Quisque tempor nisi at quam egestas, non ullamcorper ante faucibus. Aenean pretium mauris sed massa faucibus posuere. Praesent scelerisque lorem odio, id aliquet leo egestas in. Nam sed lectus commodo, sollicitudin ex nec, mollis massa. Maecenas varius ut ex id aliquet. Pellentesque ac sodales purus. Donec vel tortor est. Sed rhoncus ante sodales tellus gravida tincidunt. Nulla pulvinar nisi orci.
+            </p>
+          </div>
+        </Element>
 
-          <p className='section-body'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque cursus tortor, commodo cursus diam placerat vel. Fusce porta nulla sed felis pellentesque, id vestibulum mi mollis. Fusce dignissim lorem nec scelerisque hendrerit. Cras rutrum pharetra dolor vel feugiat. Vivamus eleifend posuere ultrices. Etiam consequat odio a aliquet iaculis. In hac habitasse platea dictumst. Maecenas sed luctus est. Duis bibendum urna a tristique volutpat.
+        <Element name="rsvp" className="section section-2">
+          {triangles}
 
-            Aliquam elementum vitae elit nec fringilla. Duis rhoncus odio enim, sed dignissim tellus varius iaculis. Aenean suscipit posuere rhoncus. Duis in nisi a nulla gravida aliquam ac viverra magna. Aenean pharetra tincidunt aliquet. Vestibulum tincidunt a tellus a pellentesque. Nam in bibendum velit.
+          <div className='section-content'>
+            <h1 className='section-header'>RSVP</h1>
 
-            Etiam consequat posuere mi vel sodales. Morbi blandit nibh tellus, ac vulputate ipsum posuere nec. Quisque feugiat erat vitae urna imperdiet ultricies. Quisque tempor nisi at quam egestas, non ullamcorper ante faucibus. Aenean pretium mauris sed massa faucibus posuere. Praesent scelerisque lorem odio, id aliquet leo egestas in. Nam sed lectus commodo, sollicitudin ex nec, mollis massa. Maecenas varius ut ex id aliquet. Pellentesque ac sodales purus. Donec vel tortor est. Sed rhoncus ante sodales tellus gravida tincidunt. Nulla pulvinar nisi orci.
-          </p>
-        </div>
+            <p className='section-body'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque cursus tortor, commodo cursus diam placerat vel. Fusce porta nulla sed felis pellentesque, id vestibulum mi mollis. Fusce dignissim lorem nec scelerisque hendrerit. Cras rutrum pharetra dolor vel feugiat. Vivamus eleifend posuere ultrices. Etiam consequat odio a aliquet iaculis. In hac habitasse platea dictumst. Maecenas sed luctus est. Duis bibendum urna a tristique volutpat.
 
-        <div className='section section-3'>
-          <h1 className='section-header'>ALL THE DETAILS</h1>
+              Aliquam elementum vitae elit nec fringilla. Duis rhoncus odio enim, sed dignissim tellus varius iaculis. Aenean suscipit posuere rhoncus. Duis in nisi a nulla gravida aliquam ac viverra magna. Aenean pharetra tincidunt aliquet. Vestibulum tincidunt a tellus a pellentesque. Nam in bibendum velit.
 
-          <p className='section-body'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque cursus tortor, commodo cursus diam placerat vel. Fusce porta nulla sed felis pellentesque, id vestibulum mi mollis. Fusce dignissim lorem nec scelerisque hendrerit. Cras rutrum pharetra dolor vel feugiat. Vivamus eleifend posuere ultrices. Etiam consequat odio a aliquet iaculis. In hac habitasse platea dictumst. Maecenas sed luctus est. Duis bibendum urna a tristique volutpat.
+              Etiam consequat posuere mi vel sodales. Morbi blandit nibh tellus, ac vulputate ipsum posuere nec. Quisque feugiat erat vitae urna imperdiet ultricies. Quisque tempor nisi at quam egestas, non ullamcorper ante faucibus. Aenean pretium mauris sed massa faucibus posuere. Praesent scelerisque lorem odio, id aliquet leo egestas in. Nam sed lectus commodo, sollicitudin ex nec, mollis massa. Maecenas varius ut ex id aliquet. Pellentesque ac sodales purus. Donec vel tortor est. Sed rhoncus ante sodales tellus gravida tincidunt. Nulla pulvinar nisi orci.
+            </p>
+          </div>
+        </Element>
 
-            Aliquam elementum vitae elit nec fringilla. Duis rhoncus odio enim, sed dignissim tellus varius iaculis. Aenean suscipit posuere rhoncus. Duis in nisi a nulla gravida aliquam ac viverra magna. Aenean pharetra tincidunt aliquet. Vestibulum tincidunt a tellus a pellentesque. Nam in bibendum velit.
+        <Element name='about-us' className='section section-3'>
+          {triangles}
 
-            Etiam consequat posuere mi vel sodales. Morbi blandit nibh tellus, ac vulputate ipsum posuere nec. Quisque feugiat erat vitae urna imperdiet ultricies. Quisque tempor nisi at quam egestas, non ullamcorper ante faucibus. Aenean pretium mauris sed massa faucibus posuere. Praesent scelerisque lorem odio, id aliquet leo egestas in. Nam sed lectus commodo, sollicitudin ex nec, mollis massa. Maecenas varius ut ex id aliquet. Pellentesque ac sodales purus. Donec vel tortor est. Sed rhoncus ante sodales tellus gravida tincidunt. Nulla pulvinar nisi orci.
-          </p>
-        </div>
+          <div className='section-content'>
+            <h1 className='section-header'>About Us</h1>
+
+            <p className='section-body'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque cursus tortor, commodo cursus diam placerat vel. Fusce porta nulla sed felis pellentesque, id vestibulum mi mollis. Fusce dignissim lorem nec scelerisque hendrerit. Cras rutrum pharetra dolor vel feugiat. Vivamus eleifend posuere ultrices. Etiam consequat odio a aliquet iaculis. In hac habitasse platea dictumst. Maecenas sed luctus est. Duis bibendum urna a tristique volutpat.
+
+              Aliquam elementum vitae elit nec fringilla. Duis rhoncus odio enim, sed dignissim tellus varius iaculis. Aenean suscipit posuere rhoncus. Duis in nisi a nulla gravida aliquam ac viverra magna. Aenean pharetra tincidunt aliquet. Vestibulum tincidunt a tellus a pellentesque. Nam in bibendum velit.
+
+              Etiam consequat posuere mi vel sodales. Morbi blandit nibh tellus, ac vulputate ipsum posuere nec. Quisque feugiat erat vitae urna imperdiet ultricies. Quisque tempor nisi at quam egestas, non ullamcorper ante faucibus. Aenean pretium mauris sed massa faucibus posuere. Praesent scelerisque lorem odio, id aliquet leo egestas in. Nam sed lectus commodo, sollicitudin ex nec, mollis massa. Maecenas varius ut ex id aliquet. Pellentesque ac sodales purus. Donec vel tortor est. Sed rhoncus ante sodales tellus gravida tincidunt. Nulla pulvinar nisi orci.
+            </p>
+          </div>
+        </Element>
       </div>
     );
   }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-scroll'
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import MenuIcon from '../../images/menu-button.svg';
@@ -21,8 +22,28 @@ class Header extends Component {
     return (
       <div className={headerClass}>
         <div className='logo'>J &amp; K</div>
-        <div className='menu'>
+        <div className='menu-mobile'>
           <img className='menu-icon' src={MenuIcon} alt='Menu'/>
+        </div>
+
+        <div className='menu-desktop'>
+          <ul className='menu-list'>
+            <li className='menu-item'>
+              <Link to="details" smooth={true} offset={-20} duration={750}>
+                Details
+              </Link>
+            </li>
+            <li className='menu-item'>
+              <Link to="rsvp" smooth={true} offset={-20} duration={750}>
+                RSVP
+              </Link>
+            </li>
+            <li className='menu-item'>
+              <Link to="about-us" smooth={true} offset={-20} duration={750}>
+                About Us
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     );
